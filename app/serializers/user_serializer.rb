372450884086fba66639356e19c763a75ce6d7fd
Jullie8 <julissale8@gmail.com>
2019-04-28 01:11:09 -0400

    serializer & seed

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
+  has_many :votes
+  has_many :ideas, through: :votes 
   attributes :id, :username, :email, :full_name, :role_title, :photo_url, :team_id
 end
