f559ef8e39a78a9c54e48adfa75d69bf6fd8a3f3
github username <raquel.sae.randall@gmail.com>
2019-04-28 01:30:47 -0400

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@ Vote.destroy_all
 User.create(username: "admin", full_name: "Administrator", role_title: "Admin", photo_url: "none", team_id: nil, email: "admin@admin.com")
 User.create(username: "Guest", full_name: "Hack", role_title: "participant", photo_url: "none", team_id: nil, email: "guest@guest.com")
 
-Idea.create(title:"Tech Recycle", topic:"Environment", description:"Recycling for good", problem:"We so dirty", solution:"Web app ... we so clean", audience:"people on earth")
-Idea.create(title:"Tech Security", topic:"Data Security", description:"Security for good", problem:"We so dangerous", solution:"Mobile app ... we so safe ", audience:"people on earth")
-Idea.create(title:"Tech Jobs", topic:"Jobs", description:"Jobs for good", problem:"We so poor", solution:"Chrome extension ... we so rich", audience:"people on earth")
-Idea.create(title:"Tech Transportation", topic:"Transit", description:"Transit for good", problem:"We so slow", solution:"Web app ... we so fast", audience:"people on earth")
+Idea.create(title:"Tech Recycle", topic:"Environment", problem:"We so dirty", solution:"Web app ... we so clean", audience:"people on earth")
+Idea.create(title:"Tech Security", topic:"Data Security", problem:"We so dangerous", solution:"Mobile app ... we so safe ", audience:"people on earth")
+Idea.create(title:"Tech Jobs", topic:"Jobs", problem:"We so poor", solution:"Chrome extension ... we so rich", audience:"people on earth")
+Idea.create(title:"Tech Transportation", topic:"Transit", problem:"We so slow", solution:"Web app ... we so fast", audience:"people on earth")
 
 
 Vote.create(user_id:2, idea_id:1)
