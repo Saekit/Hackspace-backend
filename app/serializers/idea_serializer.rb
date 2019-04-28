cdd745b122a0fff134d589b45896ecea05a0115c
rob <robertchen234@gmail.com>
2019-04-28 05:32:41 -0400

    ideas serializer

--- app/serializers/idea_serializer.rb
+++ app/serializers/idea_serializer.rb
@@ -1,3 +1,5 @@
 class IdeaSerializer < ActiveModel::Serializer
+  has_many :votes
+  has_many :users, through: :votes 
   attributes :id, :title, :topic, :problem, :solution, :audience, :full_name
 end
